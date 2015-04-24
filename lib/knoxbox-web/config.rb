84ef15afed18c47b0d3443dce740df3310754aef
Mike Mackintosh <m@zyp.io>
2015-04-24 13:55:58 -0400

    cleanup the config module

--- lib/knoxbox-web/config.rb
+++ lib/knoxbox-web/config.rb
@@ -1,15 +1,40 @@
-require 'set'
-require 'yaml'
+# Require deps
+
 
 # Define the config module
 module KnoxBoxWeb
   module Config
+    
+    require 'set'
+    require 'yaml'
+
     class << self
+
+      attr_writer :config
+
       def load(file)
         if File.exists?(file)
-          puts Yaml.load(file).inspect
+          @config = YAML.load(File.read(file))
         end
+
+        @config.symbolize_keys_deep!
       end
+
+      def get(key)
+        env = KnoxBoxWeb::Application.environment.to_sym
+        return @config[env][key.to_sym] unless @config[env][key.to_sym].nil?
+        return @config[:global][key.to_sym] unless @config[:global][key.to_sym].nil?
+        nil
+      end
+
+      def set(key, value)
+        @config.set(key, value)
+      end
+
+      def to_h
+        @config
+      end
+
     end
   end
 end
